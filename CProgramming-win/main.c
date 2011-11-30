c29d464e0d720128cfa9a85699a3b93259df5c75
IamTTFan <iamttfan@126.com>
2011-11-30 14:21:03 +0800

    first commit

---
+++ CProgramming-win/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 2011-11-21
+ *      Author: libo
+ */
+
+#include <stdio.h>
+
+int main() {
+
+	int c;
+	while((c=getchar()) != EOF) {
+		putchar(c);
+	}
+	return 0;
+}
+
